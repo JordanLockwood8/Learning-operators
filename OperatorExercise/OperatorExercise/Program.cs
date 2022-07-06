243e48e03cff16e5e5d5fc589393b3c00a9862c4
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-06 13:07:46 -0600

    learning operators

---
+++ OperatorExercise/OperatorExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using OperatorExercise;
+//excersize1
+var a = 17;
+var b = 4;
+var quotent = a / b;
+var remainder = a % b;
+Console.WriteLine($"{a}/{b} is {quotent} remainder {remainder}");
+
+//excersise2
+Console.WriteLine("what is the radius of your circle");
+var userInput = double.Parse(Console.ReadLine());
+var answer = Operators.AreaOfCircle(userInput);
+Console.WriteLine($"The area of a circle with a radius of {userInput} is {answer}");
+
